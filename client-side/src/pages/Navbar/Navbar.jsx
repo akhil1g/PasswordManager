45feb646982b3773cf67f9ce9cba7a0b47ca181d
akhil1g <akhil.gupta.12817@gmail.com>
2023-07-05 15:17:01 +0530

    adding components

---
+++ client-side/src/pages/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './navbar.css'
+
+export default function Navbar()
+{
+    return (
+        <div className="nav-box">
+            <div className="nav-heading">PassWord Manager</div>
+        </div>
+    );
+}
