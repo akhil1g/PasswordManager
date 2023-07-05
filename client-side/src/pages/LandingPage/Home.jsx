293e1da044df66dde3e11ef99ccfe37f8c853d60
aaryak4554 <joinaarya30@gmail.com>
2023-07-06 02:01:55 +0530

    Landing Page

---
+++ client-side/src/pages/LandingPage/Home.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+// import Header from "./Header";
+// import Footer from "./Footer";
+function Home() {
+  return (
+    <div className="root">
+      {/* <Header /> */}
+      <h1>Hello, User</h1>
+      <h2>What would you like to do?</h2>
+      <div className="root-container">
+        <div className="child-container ">
+          <a href="kfknvv">Store a Password</a>
+        </div>
+        <div className="child-container ">
+          <a href="efm ve">Retrieve a Password</a>
+        </div>
+        <div className="child-container">
+          <a href="vrdf">Generate a Password</a>
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+export default Home;
