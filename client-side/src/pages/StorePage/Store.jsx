2ad1967d14c25017877d00b738b374144a350194
aaryak4554 <joinaarya30@gmail.com>
2023-07-06 03:18:22 +0530

    added navbar

--- client-side/src/pages/StorePage/Store.jsx
+++ client-side/src/pages/StorePage/Store.jsx
@@ -1,55 +1,60 @@
 import React, { useState } from "react";
 import "./store.css";
+import Navbar from "../Navbar/Navbar";
 
 function Store() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [notes, setNotes] = useState("");
-  const handleSubmit = () => {
-    setUsername("");
-    setPassword("");
-    setNotes("");
-  };
-
-  return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <h2>Store a Password</h2>
-        <div className="sub">
-          <label>Username: </label>
-          <input
-            type="text"
-            id="username"
-            placeholder="username"
-            value={username}
-            onChange={(x) => setUsername(x.target.value)}
-          />
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [notes, setNotes] = useState("");
+    const handleSubmit = () => {
+      setUsername("");
+      setPassword("");
+      setNotes("");
+    };
+  
+    return (
+      <div>
+        <Navbar />
+        <div className="container">
+          <form onSubmit={handleSubmit}>
+            <h2>Store a Password</h2>
+            <div className="sub">
+              <label>Username: </label>
+              <input
+                type="text"
+                id="username"
+                placeholder="username"
+                value={username}
+                onChange={(x) => setUsername(x.target.value)}
+              />
+            </div>
+            <div className="sub">
+              <label htmlFor="password">Password: </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="***********"
+                value={password}
+                onChange={(x) => setPassword(x.target.value)}
+              />
+            </div>
+            <div className="sub">
+              <label htmlFor="notes">Add a Note: </label>
+              <input
+                type="text"
+                id="note"
+                placeholder="add a note...."
+                value={notes}
+                onChange={(x) => setNotes(x.target.value)}
+              />
+            </div>
+  
+            <button type="submit">Store</button>
+          </form>
         </div>
-        <div className="sub">
-          <label htmlFor="password">Password: </label>
-          <input
-            type="password"
-            id="password"
-            placeholder="***********"
-            value={password}
-            onChange={(x) => setPassword(x.target.value)}
-          />
-        </div>
-        <div className="sub">
-          <label htmlFor="notes">Add a Note: </label>
-          <input
-            type="text"
-            id="note"
-            placeholder="add a note...."
-            value={notes}
-            onChange={(x) => setNotes(x.target.value)}
-          />
-        </div>
-
-        <button type="submit">Store</button>
-      </form>
-    </div>
-  );
-}
-
-export default Store;
+      </div>
+    );
+  }
+  
+  export default Store;
+  
