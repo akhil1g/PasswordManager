1d2f0063291785f770c5641f7131e341d7d083cf
aaryak4554 <joinaarya30@gmail.com>
2023-07-10 14:53:59 +0530

    generate page

--- client-side/src/pages/GeneratePage/Generate.jsx
+++ client-side/src/pages/GeneratePage/Generate.jsx
@@ -1,64 +1,106 @@
-import React from "react";
-import "./generate.css";
-function Generate() {
-  return (
-    <div className="container">
-      <h3>Password Generator</h3>
-      <div className="display-container">
-        <input
-          readonly
-          placeholder="***********"
-          className="display"
-          data-passwordDisplay
-        />
-        {/* <button className="copyBtn" data-copy>
-          <span className="tooltip" data-copyMsg></span>
-          <img src="./assets/copy.svg" alt="copy" width="23" height="23" />
-        </button> */}
-      </div>
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FaClipboard, FaTheRedYeti } from 'react-icons/fa';
+import { Form } from './form';
+import { getRandomChar, getSpecialChar } from './helper';
+import Navbar from '../Navbar/Navbar';
+import './generate.css'
 
-      <div className="input-container">
-        {/* <!--password length section--> */}
-        <div className="length-container">
-          <p>Password Length</p>
-          <p data-lengthNumber>0</p>
-        </div>
-        {/* <!--slider--> */}
-        <input
-          type="range"
-          min="1"
-          max="20"
-          className="slider"
-          step="1"
-          data-lengthSlider
-        />
-        {/* <!-- checkboxes--> */}
-        <div className="check">
-          <input type="checkbox" id="uppercase" />
-          <label for="uppercase">Includes Uppercase letters</label>
-        </div>
-        <div className="check">
-          <input type="checkbox" id="lowercase" />
-          <label for="lowercase">Includes Lowercase letters</label>
-        </div>
-        <div className="check">
-          <input type="checkbox" id="numbers" />
-          <label for="numbers">Includes Numbers </label>
-        </div>
-        <div className="check">
-          <input type="checkbox" id="symbols" />
-          <label for="symbols">Includes Symbols </label>
-        </div>
-        {/* <!-- strength section--> */}
-        <div className="strength-container">
-          <p>Strength</p>
-          <div className="indicator" data-indicator></div>
-        </div>
-        {/* <!-- generate password--> */}
-        <button className="generateButton">Generate Password</button>
+function Generate() {
+  const [values, setValues]=Form({
+    length: 6,
+    capital: true,
+    small: true,
+    number: false,
+    symbol: false,
+  });
+  const [result, setResult] = useState("");
+  const fieldsArray = [
+    {
+      field: values.capital,
+      getChar: () => getRandomChar(65, 90),
+    },
+    {
+      field: values.small,
+      getChar: () => getRandomChar(97, 122),
+    },
+    {
+      field: values.number,
+      getChar: () => getRandomChar(48, 57),
+    },
+    {
+      field: values.symbol,
+      getChar: () => getSpecialChar(),
+    },
+  ];
+
+  const handleOnSubmit=function(event)
+    {
+        event.preventDefault();
+        let generatedpw="";
+        const CheckArray=fieldsArray.filter(({field})=>field)
+        for(let i=0;i<values.length;i++)
+        {
+            const index=Math.floor(Math.random()*CheckArray.length)
+            const letter=CheckArray[index]?.getChar();
+            if(letter)
+            generatedpw+=letter;
+        }
+        if(generatedpw)
+        setResult(generatedpw);
+    }
+
+  const handleClipboard = async () => {
+    if (result) {
+      await navigator.clipboard.writeText(result);   //writing on clipboard
+      toast("Copied to your clipboard");
+    } else {
+      toast('Nothing to copy');
+    }
+  };
+
+  return (
+    <div>
+    <Navbar />
+    <h1>Generate PassWord</h1>
+    <div className='outer'>
+      <div className="container">
+        <form onSubmit={handleOnSubmit}>
+          <div className="result">
+            <input type="text" className="output" placeholder="Minimum 6 Characters" readOnly value={result} />
+            <div className="clipboard" onClick={handleClipboard}>
+              <FaClipboard></FaClipboard>
+            </div>
+          </div>
+          <div>
+            <div className="field">
+              <label htmlFor="length">Length</label>
+              <input type="number" id="length" min={6} max={20} name="length" value={values.length} onChange={setValues} />
+            </div>
+            <div className="field">
+              <label htmlFor="capital">Uppercase</label>
+              <input type="checkbox" id="capital" name="capital" checked={values.capital}onChange={setValues} />
+            </div>
+            <div className="field">
+              <label htmlFor="small">Lowercase</label>
+              <input type="checkbox" id="small" name="small" checked={values.small} onChange={setValues}/>
+            </div>
+            <div className="field">
+              <label htmlFor="number">Number</label>
+              <input type="checkbox" id="number" name="number" checked={values.number} onChange={setValues}></input>
+            </div>
+            <div className="field">
+              <label htmlFor="symbol">Symbol</label>
+              <input type="checkbox" id="symbol" name="symbol" checked={values.symbol} onChange={setValues}></input>
+            </div>
+          </div>
+          <button type="submit">Generate Password</button>
+        </form>
       </div>
+      </div>
+      <ToastContainer />
     </div>
   );
 }
-
 export default Generate;
---
+++ client-side/src/pages/GeneratePage/form.js
@@ -0,0 +1,12 @@
+import React,{useState} from "react"
+export function Form(initialValues){
+    const [values,setValues]=useState(initialValues);
+    return [
+        values, function(event){
+            setValues({ 
+                ...values,   //taking previous values that we already have
+                [event.target.name] : event.target.type === 'checkbox'? event.target.checked : event.target.value,
+            });
+        }
+    ];
+}
