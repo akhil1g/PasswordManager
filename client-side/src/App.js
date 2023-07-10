6a4a96cdae9f22ba577ab7e3089ace6ab496158e
aaryak4554 <joinaarya30@gmail.com>
2023-07-10 21:17:25 +0530

    added background image

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './pages/Navbar/Navbar.jsx';
 import Main from './pages/MainPage/Main.jsx';
 import Login from './pages/LoginPage/Login.jsx';
-import Register from './pages/RegisterPage/Register.jsx';
+// import Register from './pages/RegisterPage/Register.jsx';
 import Home from './pages/LandingPage/Home.jsx';
 import Store from './pages/StorePage/Store.jsx';
 import Retrieve from './pages/RetrievePage/Retrieve.jsx';
@@ -19,11 +19,11 @@ export default function App() {
       <BrowserRouter>
       <Routes>
       <Route path='/' exact Component={Main}></Route>
-      <Route path='/login' exact Component={Login}></Route>
-      <Route path='/register' exact Component={Register}></Route>
+      {/* <Route path='/login' exact Component={Login}></Route> */}
+      {/* <Route path='/register' exact Component={Register}></Route> */}
       <Route path='/home' exact Component={Home}></Route>
       <Route path='/store' exact Component={Store}></Route>
-      <Route path='/retrieve' exact Component={Retrieve}></Route>
+      {/* <Route path='/retrieve' exact Component={Retrieve}></Route> */}
       <Route path='/generate' exact Component={Generate}></Route>
       </Routes>
       </BrowserRouter>
