650a41a81c3d7a93b1a364fddf9af4e1ace263a4
aaryak4554 <joinaarya30@gmail.com>
2023-07-10 18:46:04 +0530

    Main page

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter,Route, Routes} from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 import Navbar from './pages/Navbar/Navbar.jsx';
+import Main from './pages/MainPage/Main.jsx';
 import Login from './pages/LoginPage/Login.jsx';
-// import Register from './pages/RegisterPage/Register.jsx';
-import Store from './pages/StorePage/Store.jsx';
+import Register from './pages/RegisterPage/Register.jsx';
 import Home from './pages/LandingPage/Home.jsx';
+import Store from './pages/StorePage/Store.jsx';
+import Retrieve from './pages/RetrievePage/Retrieve.jsx';
 import Generate from './pages/GeneratePage/Generate.jsx';
 
 export default function App() {
@@ -15,13 +18,15 @@ export default function App() {
       <Navbar />
       <BrowserRouter>
       <Routes>
+      <Route path='/' exact Component={Main}></Route>
       <Route path='/login' exact Component={Login}></Route>
       <Route path='/register' exact Component={Register}></Route>
       <Route path='/home' exact Component={Home}></Route>
+      <Route path='/store' exact Component={Store}></Route>
+      <Route path='/retrieve' exact Component={Retrieve}></Route>
       <Route path='/generate' exact Component={Generate}></Route>
       </Routes>
       </BrowserRouter>
-      {/* <Store /> */}
     </div>
   )
 }
--- client-side/src/pages/MainPage/Main.jsx
+++ client-side/src/pages/MainPage/Main.jsx
@@ -1,39 +1,47 @@
 import React from "react";
 import "./Main.css";
+// import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-// import bgimage from "./bg.webp";
 
 function Main() {
+
+    const navigate = useNavigate();
+    function handleLogin(){
+        navigate('./Login');
+    }
+    function handleRegister(){
+        navigate('./Register');
+    }
+
   return (
     <div className="container">
-      <div
-        className="left-sec"
-        // style={{
-        //   width: `100%`,
-        //   height: `100%`,
-        //   backgroundImage: `url(${bgimage})`
-        // }}
-      >
-        <h3>About Password Managers</h3>
+      <div className="left-sec" >
+        <h1>Password Manager</h1>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
-        </p>
-        <p>
+          industry.Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book.
           Lorem Ipsum has been the industry's standard dummy text ever since the
           1500s, when an unknown printer took a galley of type and scrambled it
           to make a type specimen book.
         </p>
       </div>
       <div className="right-sec">
-        <div className="welcome-message">
-          <h3>Welcome!</h3>
-          <p>Get started by either logging in or signing up.</p>
-        </div>
-        <div className="buttons-container">
-          <button className="login-btn">Login</button>
-          <button className="register-btn">Register</button>
-        </div>
+        <h3>Welcome!</h3>
+        <p>New here? </p>
+        <p>Get started by creating an account with us.</p>
+        <button 
+            className="register-btn"
+            onClick={(e) => handleRegister()}
+        >Register</button>
+        <p>Already an User?</p>
+        <p>Click below to continue.</p>
+        <button 
+            className="login-btn"
+            onClick={handleLogin}
+        >Login</button>
       </div>
     </div>
   );
